3218fe107d628b8de0be80755a6e64157fb46f82
biyamn <b1y9mn@gmail.com>
2024-05-11 11:32:22 +0900

    feat: 채팅룸 api 추가

--- src/pages/api/chat-room.ts
+++ src/pages/api/chat-room.ts
@@ -1,34 +1,39 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { SupabaseServer } from "@/supabase";
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { SupabaseServer } from '@/supabase';
 
 export default async function ChatRoom(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   switch (req.method) {
-    case "GET": {
-      const { data: rooms } = await SupabaseServer.from("ChatRoom")
-        .select("id, room_name")
-        .order("createdAt", { ascending: false });
+    case 'GET': {
+      const { data: rooms } = await SupabaseServer.from('ChatRoom')
+        .select('id, room_name')
+        .order('createdAt', { ascending: false });
 
       return res.status(200).json({
         rooms,
       });
     }
 
-    case "POST": {
-      // TODO(@biyamn): 채팅방 생성하는 기능 만들기
+    case 'POST': {
+      const { data } = await SupabaseServer.from('ChatRoom')
+        .insert({
+          room_name: req.body.room_name,
+        })
+        .select('id')
+        .single();
       return res.status(200).json({
-        roomId: "",
+        roomId: data?.id,
       });
     }
 
-    case "DELETE": {
-      // TODO(@biyamn): 채팅방 삭제하는 기능 만들기
-      return res.status(200).send("ok");
+    case 'DELETE': {
+      await SupabaseServer.from('ChatRoom').delete().eq('id', req.body.id);
+      return res.status(200).send('ok');
     }
 
     default:
-      return res.status(405).send("method not allowed");
+      return res.status(405).send('method not allowed');
   }
 }
